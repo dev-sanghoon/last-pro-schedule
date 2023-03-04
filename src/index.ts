f092ca85dfe562ea4233f5a8b270e16d55f56914
dev-sanghoon <dev.sanghoon@gmail.com>
2023-03-04 15:04:46 +0900

    feat: design user apis

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,27 @@ dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("done");
+  res.send("Hello Habitier");
+});
+
+app.post("/users", (req, res) => {
+  res.send("register");
+});
+
+app.delete("/users", (req, res) => {
+  res.send("unregister");
+});
+
+app.post("/users/login", (req, res) => {
+  res.send("login");
+});
+
+app.post("/users/logout", (req, res) => {
+  res.send("logout");
+});
+
+app.post("/users/check", (req, res) => {
+  res.send("search registered & ID & password");
 });
 
 app.listen(process.env.PORT, () => {
