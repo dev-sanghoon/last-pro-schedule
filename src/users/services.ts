514de0f364267c1813051eebd94111f8bea96a56
dev-sanghoon <dev.sanghoon@gmail.com>
2023-03-06 14:13:57 +0900

    feat: implement login via jwt

--- src/users/services.ts
+++ src/users/services.ts
@@ -1,3 +1,4 @@
+import jsonwebtoken from "jsonwebtoken";
 import { Request, Response } from "express";
 import * as users from "./models";
 
@@ -58,7 +59,36 @@ export async function unregister(req: Request, res: Response) {
   }
 }
 
-export function login(req: Request, res: Response) {}
+export async function login(req: Request, res: Response) {
+  try {
+    if (!process.env.JWT_SECRET) {
+      res.status(500).json({ success: false, message: "Unexpected" });
+      return;
+    }
+    const queried = await users.verifyPassword(req.body.email);
+    if (queried.length === 0) {
+      res.status(400).json({ success: false, message: "Email not found" });
+      return;
+    }
+    const [target] = queried;
+    if (target.password === req.body.password) {
+      const payload = { email: req.body.email };
+      const createdToken = jsonwebtoken.sign(payload, process.env.JWT_SECRET);
+      res.append(
+        "Set-Cookie",
+        `access_token=${createdToken}; Path=/; Max-Age=3600; HttpOnly`
+      );
+      res.status(200).json({ success: true, data: payload });
+      return;
+    }
+    res
+      .status(500)
+      .json({ success: false, message: "Might be wrong password" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: "Unexpected" });
+  }
+}
 
 export function logout(req: Request, res: Response) {}
 
