944fe52c7a0bad7e56c7b1498c192701a6db4ac3
dev-sanghoon <dev.sanghoon@gmail.com>
2023-03-06 12:07:28 +0900

    fix: remove unnecesssary consoles

--- src/users/models.ts
+++ src/users/models.ts
@@ -33,15 +33,11 @@ export const createUser = async (
   password: string,
   name: string
 ) => {
-  const [result] = await pool.query(
+  await pool.query(
     `INSERT INTO Users (email, password, name) VALUES ("${email}", "${password}", "${name}")`
   );
-  console.log(result);
 };
 
 export const deleteUserByEmail = async (email: string) => {
-  const [result] = await pool.query(
-    `DELETE FROM Users WHERE email = "${email}"`
-  );
-  console.log(result);
+  await pool.query(`DELETE FROM Users WHERE email = "${email}"`);
 };
