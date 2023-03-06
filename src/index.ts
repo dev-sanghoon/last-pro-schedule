f4b4408a271bb2dfb5e6529091d1f39604db6910
dev-sanghoon <dev.sanghoon@gmail.com>
2023-03-06 16:30:59 +0900

    feat: use parser for jwt

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cookieParser from "cookie-parser";
 import users from "./users/controllers";
 import "./db";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello Habitier");
